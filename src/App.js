0c05b2410208b437f2dff703d046bf343eea9a66
tlin108 <tlin108@hotmail.com>
2018-05-31 18:45:13 -0400

    add more measurement

--- src/App.js
+++ src/App.js
@@ -10,17 +10,50 @@ const measurementDic = {
 	teaSpoon: {
 		teaSpoon: 1,
 		tableSpoon: (1 / 3),
-		cup: ( 1 / 48 )
+		cup: (1 / 48),
+		fluidOunce: (1 / 6),
+		pint: (1 / 96),
+		quart: (1 / 192)
 	},
 	tableSpoon: {
 		teaSpoon: 3,
 		tableSpoon: 1,
-		cup: (1 / 16)
+		cup: (1 / 16),
+		fluidOunce: (1 / 2),
+		pint: (1 / 32),
+		quart: (1 / 64)
 	},
 	cup: {
 		teaSpoon: 48,
 		tableSpoon: 16,
-		cup: 1
+		cup: 1,
+		fluidOunce: 8,
+		pint: (1 / 2),
+		quart: (1 / 4)
+	},
+	fluidOunce: {
+		teaSpoon: 6,
+		tableSpoon: 2,
+		cup: (1 / 8),
+		fluidOunce: 1,
+		pint: (1 / 16),
+		quart: (1 / 32)
+	},
+	pint: {
+		teaSpoon: 96,
+		tableSpoon: 32,
+		cup: 2,
+		fluidOunce: 16,
+		pint: 1,
+		quart: (1 / 2)
+	},
+	quart: {
+		teaSpoon: 192,
+		tableSpoon: 64,
+		cup: 4,
+		fluidOunce: 32,
+		pint: 2,
+		quart: 1
 	}
 }
 
@@ -36,6 +69,18 @@ const measurement = [
 	{
 		label: 'Cup',
 		value: 'cup'
+	},
+	{
+		label: 'Fluid Ounce',
+		value: 'fluidOunce'
+	},
+	{
+		label: 'Pint',
+		value: 'pint'
+	},
+	{
+		label: 'Quart',
+		value: 'quart'
 	}
 ]
 
@@ -88,32 +133,32 @@ const wholeQuantity = [
 
 const fractionQuantity = [
 	{
-		label: '0',
+		label: '',
 		value: 0
 	},
 	{
 		label: '1/8',
-		value: 0.125
+		value: (1 / 8)
 	},
 	{
 		label: '1/4',
-		value: 0.25
+		value: (1 / 4)
 	},
 	{
 		label: '1/3',
-		value: 0.33
+		value: (1 / 3)
 	},
 	{
 		label: '1/2',
-		value: 0.5
+		value: (1 / 2)
 	},
 	{
 		label: '2/3',
-		value: 0.67
+		value: (2 / 3)
 	},
 	{
 		label: '3/4',
-		value: 0.75
+		value: (3 / 4)
 	}
 ]
 
@@ -150,7 +195,7 @@ export default class App extends React.Component {
 					</View>
 				</View>
 				<View>
-					<Text style={{ textAlign: 'center', fontSize: 24 }}>{ convert(quantity + fraction , measurementDic[from][to]) }</Text>
+					<Text style={{ textAlign: 'center', fontSize: 48 }}>{ convert(quantity + fraction , measurementDic[from][to]) }</Text>
 					<Picker
 						selectedValue={ to }
 						onValueChange={ value => this.setState({ to: value })}
